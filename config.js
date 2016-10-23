65ff8f46a42b5c38858a21bb99b95ef75eee70b8
RoberTu <robertu0717@gmail.com>
2016-10-23 15:14:07 +0800

    Add logger; Fix bugs; Improve codes;
    v0.2.0

---
+++ config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+// Console
+const DIVIDER_STR = '-'.repeat(process.stdout.columns);
+
+// Path
+const ROOT_PATH = path.resolve(__dirname);
+const ASSETS_PATH = path.resolve(ROOT_PATH, 'assets');
+const DB_PATH = path.resolve(ROOT_PATH, 'db.json');
+const ICON_PATH = path.resolve(ASSETS_PATH, 'logo.jpg');
+const PRODUCT_IMAGE_PATH = path.resolve(ASSETS_PATH, 'product.jpeg');
+
+// Request
+const REQUEST_HEADER = {
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+};
+const REQUEST_OPTIONS = {
+  keyword: 'iphone 6s 64g',
+  priceStart: 2000,
+  priceEnd: 3300,
+};
+const REQUEST_INTERVAL = 1000 * 60 * 3; // 2min
+
+
+// Exports
+exports.DIVIDER_STR = DIVIDER_STR;
+
+exports.DB_PATH = DB_PATH;
+exports.ICON_PATH = ICON_PATH;
+exports.PRODUCT_IMAGE_PATH = PRODUCT_IMAGE_PATH;
+
+exports.REQUEST_HEADER = REQUEST_HEADER;
+exports.REQUEST_OPTIONS = REQUEST_OPTIONS;
+exports.REQUEST_INTERVAL = REQUEST_INTERVAL;
--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const request = require('./lib/request');
+const { REQUEST_OPTIONS, REQUEST_INTERVAL } = require('./config.js');
+const { requestWebpage, downloadProductPic } = require('./lib/request');
 const parse = require('./lib/parse');
 const save = require('./lib/save');
 const diff = require('./lib/diff');
 const notifier = require('./lib/notifier');
-
-// CONFIG
-const REQUEST_OPTIONS = {
-  keyword: 'iphone 6s 64g',
-  priceStart: 2000,
-  priceEnd: 3300,
-};
-const INTERVAL = 1000 * 10; // 2min
+const log = require('./lib/log');
 
 // Start
 (function start() {
-  request(REQUEST_OPTIONS)
+  console.log(((new Date()).toLocaleString()));
+
+  requestWebpage(REQUEST_OPTIONS)
   .then(parse)
   .then(save)
   .then(diff)
+  .then(downloadProductPic)
+  .then(log)
   .then(notifier)
   .catch(err => console.error(err));
 
-  setInterval(start, INTERVAL);
+  setTimeout(start, REQUEST_INTERVAL);
 }());
